fd9ed6d9f9cf7047c9718f6e66dddaacbf27a50d
Matt Mets <matt@blinkinlabs.com>
2015-03-30 08:06:44 -0400

    Update driver signer, re-sign drivers

--- avr/driver/driver_signer.bat
+++ avr/driver/driver_signer.bat
@@ -8,10 +8,11 @@
 
 
 :: Path to the Windows SDK/ Driver SDK
+set inftocatpath="C:\Program Files (x86)\Windows Kits\8.0\bin\x86\"
 set toolpath="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\"
 
 :: Make a .cat file for the .inf file
-%toolpath%inf2cat /v /driver:%CD% /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64
+%inftocatpath%inf2cat /v /driver:%CD% /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64
 
 :: Sign the .cat file, using the GlobalSign root certificate and the private certificate
 :: Note: First, add the private certificate to the Windows install!
